b18014726530e188334ae9bc59929fe13dd2ce61
trev <trev.dodds@gmail.com>
2011-02-18 14:23:44 +0100

    Added libs and CMake files

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <cstdlib>
+#include "getopt_basic.h"
+#include <iostream>
+#include "udpraw.h"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+  cout << "Hello" << endl;
+
+  return 0;
+}
+
